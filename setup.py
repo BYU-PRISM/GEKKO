8c24f151c85b7702f5d8c85572c317015860b9a6
Logan Beal <beal.logan@gmail.com>
2018-03-12 11:57:00 -0600

    Change manifest and setup to correctly include GUI files. Release 0.0.4a9

--- setup.py
+++ setup.py
@@ -9,6 +9,18 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+#gather all files for GUI support
+gui_files = package_files('gekko/static')
+#add APM executable file
+extra_files = gui_files + ['bin/apm.exe']
+
 #APM binaries based on OS (currently only available for Windows)
 #if os.name == 'nt':
 #    apm_binary = {'gekko': ['bin/apm.exe']}
@@ -18,7 +30,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 #   apm_binary = ['gekko/bin/apmonitor' AND LA libaries]
 
 setup(name='gekko',
-    version='0.0.4a8',
+    version='0.0.4a9',
     description='Optimization software for differential algebraic equations',
     long_description=long_description,
     #url="https://readthedocsurl",
@@ -43,8 +55,9 @@ setup(name='gekko',
         'numpy>=1.8'#,
         #'ujson',
     ],
-    include_package_data=True,
-#    package_data=apm_binary,
+#    include_package_data=True,
+#    package_dir={'':'gekko'},
+    package_data={'gekko': extra_files},#['bin/apm.exe','static/*']},
 #   TODO add testing
 #    test_suite='pytest.collector',
 #    tests_require=['pytest'],
