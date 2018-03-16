d2e14611ef0a3ce7f5551ee58f65376283ed3998
Daniel Hill <dhill2522@gmail.com>
2018-03-15 19:10:30 -0600

    updated build script to automatically transfer built files

--- gekko/gui/build/build.js
+++ gekko/gui/build/build.js
@@ -10,9 +10,47 @@ const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
+const fs = require('fs')
 
 const spinner = ora('building for production...')
 spinner.start()
+
+function copyFileSync( source, target ) {
+
+    var targetFile = target;
+
+    //if target is a directory a new file with the same name will be created
+    if ( fs.existsSync( target ) ) {
+        if ( fs.lstatSync( target ).isDirectory() ) {
+            targetFile = path.join( target, path.basename( source ) );
+        }
+    }
+
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function copyFolderRecursiveSync( source, target ) {
+    var files = [];
+
+    //check if folder needs to be created or integrated
+    var targetFolder = path.join( target, path.basename( source ) );
+    if ( !fs.existsSync( targetFolder ) ) {
+        fs.mkdirSync( targetFolder );
+    }
+
+    //copy
+    if ( fs.lstatSync( source ).isDirectory() ) {
+        files = fs.readdirSync( source );
+        files.forEach( function ( file ) {
+            var curSource = path.join( source, file );
+            if ( fs.lstatSync( curSource ).isDirectory() ) {
+                copyFolderRecursiveSync( curSource, targetFolder );
+            } else {
+                copyFileSync( curSource, targetFolder );
+            }
+        } );
+    }
+}
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
@@ -37,5 +75,24 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
     ))
+    // File copy actually goes here
+    console.log('Copying built files to GEKKO static folder...')
+    // Replace the files in gekko/static with the built files from gekko/gui/dist
+    const srcDir = path.join(__dirname, '..', 'dist')
+    const destDir = path.join(__dirname, '..', '..', 'static')
+    if (fs.existsSync(destDir)) {
+      rm.sync(destDir)
+    }
+    fs.mkdirSync(destDir)
+    const builtFiles = fs.readdirSync(srcDir)
+    for (var i = 0; i < builtFiles.length; i++) {
+      // We only need to worry about one layer deep right now, so no recursion necessary
+      if (fs.statSync(path.join(srcDir, builtFiles[i])).isDirectory()) {
+        copyFolderRecursiveSync(path.join(srcDir, builtFiles[i]), destDir)
+      } else {
+        fs.copyFileSync(path.join(srcDir, builtFiles[i]), path.join(destDir, builtFiles[i]))
+      }
+    }
+    console.log('Copy complete.\n')
   })
 })
