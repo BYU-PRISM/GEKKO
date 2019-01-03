cc294d7333e05e22879aee541de7ada2ca2d8ed3
APMonitor <john@apmonitor.com>
2019-01-03 13:34:44 -0700

    Permits deepcopy

--- gekko/gk_operators.py
+++ gekko/gk_operators.py
@@ -158,11 +158,13 @@ class GK_Value(list):
         return np.array(self.value)
 
     def __iter__(self):
-        for v in self.value:
-            yield v
-        
+        try:
+            for v in self.value:
+                yield v
+        except:
+            return self.value        
             
-        #%%Operator overloading for building functions
+    #%%Operator overloading for building functions
     #comparisons
     def __lt__(self,other): #less than
         return self.value < other
