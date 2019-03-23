fb3ded4ffc73443da714f54778684e543268e2b7
APMonitor <john@apmonitor.com>
2019-03-23 13:50:33 -0600

    Update for 0.2b3

--- gekko/__init__.py
+++ gekko/__init__.py
@@ -1,13 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 11 16:31:25 2017
-
-@author: scd
-"""
-
 from .gekko import GEKKO
 #from .brain import Brain
 
 Gekko = GEKKO
 gekko = GEKKO
+
+__version__ = '0.2b3'
 
