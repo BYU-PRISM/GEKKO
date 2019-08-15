64ee5f723959921e56ca2541eff0ae2425c55895
APMonitor <john@apmonitor.com>
2019-08-15 16:30:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ extra_files = gui_files + ['bin/apm.exe','bin/apm','bin/apm_arm','bin/apm_mac']
 
 # versions: a (alpha), b (beta), rc (release candidate)
 setup(name='gekko',
-    version='0.2.2',
+    version='0.2.3',
     description='Machine learning and optimization for dynamic systems',
     long_description=long_description,
     classifiers=[
