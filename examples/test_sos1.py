5d78c2fad838813a5ab680b5799aad94a57946c2
APMonitor <john@apmonitor.com>
2019-08-18 00:49:40 -0600

    Update SOS1 example

--- examples/test_sos1.py
+++ examples/test_sos1.py
@@ -1,11 +1,6 @@
 from gekko import GEKKO
-
 m = GEKKO()
-
-y = m.sos1([1.5,2.5,0.5,8.5])
-
-m.Obj(y)
-
+y = m.sos1([19.05, 25.0, 29.3, 30.2])
+m.Obj(y) # select the minimum value
 m.solve()
-
 print(y.value)
