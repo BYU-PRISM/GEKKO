70c7d6a0c438f1a1e28fa5b1dffeed349412331a
APMonitor <john@apmonitor.com>
2019-09-12 07:00:04 -0600

    Fix if2 (remove integer=True)

--- gekko/gekko.py
+++ gekko/gekko.py
@@ -846,7 +846,7 @@ class GEKKO(object):
         Output: GEKKO variable y = x1 when condition<0
                                y = x2 when condition>=0
         """
-        # add switching (b) and output (y) variables
+        # add binary (intb) and output (y) variable
         b = self.Var(0.01,lb=0,ub=1)
         y = self.Var()
         # add equations for switching conditions
