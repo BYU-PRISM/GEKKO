678911fdfedf4eb45ba25fcbb838ea3fa7fd2ed6
APMonitor <john@apmonitor.com>
2020-07-27 20:13:47 -0500

    Add files via upload

--- examples/test_fix_free.py
+++ examples/test_fix_free.py
@@ -1,22 +1,26 @@
 from gekko import GEKKO
 import numpy as np
+import matplotlib.pyplot as plt
 
 m = GEKKO(remote=False)
 
 t = np.linspace(0,10,11)
 m.time = t
 
-p = m.Param(t+1,lb=0.5,ub=1)
+p = m.Param(t+1,lb=0.2,ub=1)
+x = m.Var(0)
+
+m.Equation(x.dt()==p)
+m.Minimize(x**2)
+m.options.IMODE = 6
 
 m.free(p)
-m.fix_initial(p,1)
-m.fix_final(p,2)
+m.fix_initial(p,0.9)
+m.fix_final(p,1.0)
+m.free_initial(x)
+#m.free_final(x)
+m.solve(disp=True)
 
-m.Minimize(p**2)
-
-m.options.IMODE = 6
-m.solve()
-p.value = t
-m.solve()
-
-print(p.value)
+plt.plot(t,p.value,'ro-')
+plt.plot(t,x.value,'bx-')
+plt.show()
