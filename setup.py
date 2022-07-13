907806494f43fc421178c4c9665f17f25113477b
APMonitor <john@apmonitor.com>
2022-07-12 18:14:39 -0600

    Update setup.py for v1.0.5

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ Gekko models consist of equations and variables that create a symbolic represent
 problem for a single data point or single time instance. Solution modes then create the full model
 over all data points or time horizon. Gekko supports a wide range of problem types, including:
 
-- Linear programming (LP)
-- Quadratic programming (QP)
-- Nonlinear programming (NLP)
-- Mixed-integer linear programming (MILP)
-- Mixed-integer quadratic programming (MIQP)
-- Mixed-integer nonlinear programming (MINLP)
-- Differential algebraic equations (DAEs)
-- Mathematical programming with complementarity constraints (MPCCs)
+- Linear Programming (LP)
+- Quadratic Programming (QP)
+- Nonlinear Programming (NLP)
+- Mixed-Integer Linear Programming (MILP)
+- Mixed-Integer Quadratic Programming (MIQP)
+- Mixed-Integer Nonlinear Programming (MINLP)
+- Differential Algebraic Equations (DAEs)
+- Mathematical Programming with Complementarity Constraints (MPCCs)
 - Data regression / Machine learning
 - Moving Horizon Estimation (MHE)
 - Model Predictive Control (MPC)
@@ -32,9 +32,10 @@ over all data points or time horizon. Gekko supports a wide range of problem typ
 - Sequential or Simultaneous DAE solution
 
 Gekko compiles the model into byte-code and provides sparse derivatives to the solver with
-automatic differentiation. Gekko is built for industrially hardened control and optimization on Windows,
-Linux, MacOS, ARM processors, or any other platform that runs Python. Options are available for local,
-edge, or cloud computing.
+automatic differentiation. Gekko includes data cleansing functions and automatic safe-mode actions for industrially 
+hardened control and optimization on Windows, Linux, MacOS, ARM processors, or any other platform that 
+runs Python. Options are available for local, edge, and cloud solutions to manage memory or compute 
+resources.
 
 """
 
@@ -53,7 +54,7 @@ extra_files = gui_files + ['bin/apm.exe','bin/apm','bin/apm_arm','bin/apm_mac']
 # versions: a (alpha), b (beta), rc (release candidate)
 # update version here, __init__.py, and create a GitHub release
 setup(name='gekko',
-    version='1.0.4',
+    version='1.0.5',
     description='Machine learning and optimization for dynamic systems',
     long_description=long_description,
     classifiers=[
@@ -70,7 +71,7 @@ setup(name='gekko',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
     ],
-    keywords='differential deep learning reinforcement optimization mixed-integer',
+    keywords='differential deep learning solver equations optimization mixed-integer',
     url='https://github.com/BYU-PRISM/GEKKO',
     author='BYU PRISM Lab',
     author_email='john_hedengren@byu.edu',
