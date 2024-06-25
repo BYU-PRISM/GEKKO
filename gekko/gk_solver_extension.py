5c7a1c2400015af3978101e2ccd9337cd64bbd8e
Alma Walmsley <almawalmsley@gmail.com>
2024-06-26 10:26:01 +1200

    update handle_status method to set SOLVESTATUS after solve

--- gekko/gk_solver_extension.py
+++ gekko/gk_solver_extension.py
@@ -270,10 +270,15 @@ class GKConverter(ABC):
         """
         handle the status of the solver
         """
+        self._gekko_model.options.SOLVESTATUS = 1  # OK
         match status_dict.get(status, "error"):
-            case "ok": pass
-            case "warning": print("WARNING: %s" % (status_string))
-            case "error": raise Exception("@error: %s" % (status_string))
+            case "ok":
+                pass
+            case "warning": 
+                print("WARNING: %s" % (status_string))
+            case "error": 
+                self._gekko_model.options.SOLVESTATUS = 0  # Error
+                raise Exception("@error: %s" % (status_string))
     
 
     def store(self) -> None:
